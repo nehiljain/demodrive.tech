af847d38152000cb1a1c99dfd26a747fabde86d4
Nehil Jain <2761999+nehiljain@users.noreply.github.com>
2025-01-09 21:11:54 -0500

    feat(analytics): Added posthog analytics

---
+++ app/PostHogPageView.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect, Suspense } from "react"
+import { usePostHog } from 'posthog-js/react'
+
+function PostHogPageView() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const posthog = usePostHog()
+
+  // Track pageviews
+  useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`
+      }
+
+      posthog.capture('$pageview', {
+        '$current_url': url
+      })
+    }
+  }, [pathname, searchParams, posthog])
+  
+  return null
+}
+
+// Wrap in Suspense to avoid client-side rendering of the whole app
+export default function SuspendedPostHogPageView() {
+  return (
+    <Suspense fallback={null}>
+      <PostHogPageView />
+    </Suspense>
+  )
+} 
