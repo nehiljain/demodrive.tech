52de9227907914b7e07d7ac714a7efc98a71f541
Nehil Jain <2761999+nehiljain@users.noreply.github.com>
2025-02-04 12:05:41 -0800

    feat(landing_page): update metadata and enhance Open Graph for branding alignment - change title for improved relevance - add Open Graph metadata for social sharing - update Twitter image reference - include image for Open Graph with dimensions and alt text - ensure consistency in metadata across platforms - add metadataBase URL for SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,24 +15,39 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "DemoDrive AI - Video and Docs Creator",
+  title: "DemoDrive AI - copilot for technical GTM workflows",
   description: "Create stunning videos and guides with AI.",
   keywords: "video and docs creator, AI video and docs, technical writing, documentation improvement, API docs, developer experience",
   authors: [{ name: "DemoDrive AI" }],
   creator: "DemoDrive AI",
   publisher: "DemoDrive AI",
+  metadataBase: new URL("https://demodrive.tech"),
   twitter: {
     card: "summary_large_image",
     creator: "@demodrive_ai",
     title: "DemoDrive AI - Video and Docs Creator",
     description: "Create stunning videos and guides with AI.",
-    // images: ["/sample_report_landing_page_screenshot.png"],
+    images: ["/demodrive_og_image.png"],
+  },
+  openGraph: {
+    title: "DemoDrive AI - Video and Docs Creator",
+    description: "Create stunning videos and guides with AI.",
+    url: "https://demodrive.tech",
+    siteName: "DemoDrive AI",
+    images: [
+      {
+        url: "/demodrive_og_image.png",
+        width: 1200,
+        height: 630,
+        alt: "DemoDrive AI - Video and Docs Creator",
+      },
+    ],
+    type: "website",
   },
   robots: {
     index: true,
     follow: true,
   },
-  metadataBase: new URL("https://demodrive.tech"),
   icons: {
     icon: [
       { url: '/favicon.ico' },
