dccc93bf505c9a82cd13bad145f6cb74a2aef8d0
Selvam Palanimalai <selvam.palanimalai@gmail.com>
2025-02-04 11:49:27 +0530

    fix: replacing mux video, with <video>

--- components/ui/hero-video.tsx
+++ components/ui/hero-video.tsx
@@ -1,35 +1,28 @@
-import MuxPlayer from '@mux/mux-player-react'
 import { cn } from '@/lib/utils'
 
 interface HeroVideoProps {
-	playbackId: string
 	className?: string
 	title?: string
 	poster?: string
-	thumbnailTime?: number
 }
 
 export function HeroVideo({
-	playbackId,
 	className,
 	title,
-	poster,
-	thumbnailTime
+	poster
 }: HeroVideoProps) {
 	return (
 		<div className={cn('mt-16 max-w-4xl mx-auto rounded-lg overflow-hidden shadow-2xl shadow-primary/20', className)}>
-			<MuxPlayer
-				playbackId={playbackId}
-				metadataVideoTitle={title}
-				streamType="on-demand"
-				primaryColor="#ffffff"
-				secondaryColor="#3a3c40"
-				accentColor="#b1cc00"
+			<video
+				src="https://storage.googleapis.com/demodrive-media/20250203/output/DemoDrive_Product_Video_20250203_122007/Demodrive_Quick_Product_Videos_Promo.mp4"
+				title={title}
+				poster={poster}
+				controls
 				className="aspect-video w-full"
 				style={{ height: '100%', width: '100%' }}
-				poster={poster}
-				thumbnailTime={thumbnailTime}
-			/>
+			>
+				Your browser does not support the video tag.
+			</video>
 		</div>
 	)
 }
